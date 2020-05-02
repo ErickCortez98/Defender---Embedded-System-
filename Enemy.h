6b99be5b751d0edd69c13367c94dd305f39ec331
ErickCortez98 <erick-cortez98@utexas.edu>
2020-05-01 21:18:05 -0500

    added enemy class and in screen

---
+++ Enemy.h
@@ -0,0 +1,23 @@
+//Provides implementation of enemy class functions
+// Program written by: Jaxon Coward, Erick I. Cortez Valdez
+//Date created: 5/01/2020
+
+#include <stdint.h>
+#include "Sprite.h"
+
+class Enemy{
+	private: 
+		Sprite enemySprite;
+		uint8_t status; //dead or alive --- 0 or 1
+		uint8_t live; // enemies staring live: 50 points of live for enemy 1, 100 points of live for enemy 2
+	public:
+		Enemy(uint8_t x, uint8_t y, uint8_t initalLive);//constructor, we specify the type of our enemy with it's initial live
+		uint8_t getX();
+		uint8_t getY();
+		uint8_t getStatus();
+		void setStatus(uint8_t newStatus);
+		uint8_t getLive();
+		void reduceLive(uint8_t liveReduction);
+		void Draw();
+		void UpdatePos(uint16_t x, uint16_t y);
+};
